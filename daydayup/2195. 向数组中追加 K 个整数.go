536c71149ba3c0aa102def11877bd6e86b9a1aea
Bilibotter <1102043797@qq.com>
2022-09-04 18:25:46 +0800

    击败95.83%

--- daydayup/2195. 向数组中追加 K 个整数.go
+++ daydayup/2195. 向数组中追加 K 个整数.go
@@ -7,18 +7,18 @@ func minimalKSum(nums []int, k int) int64 {
 	if nums[0] > k {
 		return int64(k * (k + 1) / 2)
 	}
-	inner := 0
+	sum := 0
 	for i, num := range nums {
 		if i > 0 && nums[i-1] == num {
 			continue
 		} else if num <= k {
 			k += 1
-			inner += num
+			sum += num
 		} else {
 			break
 		}
 	}
-	return int64((k+1)*k/2 - inner)
+	return int64((k+1)*k/2 - sum)
 }
 
 func main() {
